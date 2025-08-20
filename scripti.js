d280bb693739b1bcc2f699a282779759495ed9e3
J.Vinícius <104866052+joavincus@users.noreply.github.com>
2025-08-20 17:35:26 -0300

    Add files via upload

---
+++ scripti.js
@@ -0,0 +1,30 @@
+  const swiper = new Swiper('.wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//   responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    768: {
+        slidesPerView: 2,
+    },
+    1024: {
+        slidesPerView: 3,
+    }
+  }
+});
